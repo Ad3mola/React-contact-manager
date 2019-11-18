895eb1f3ca22b73d5b5d3756dbcd7a701b800300
Ad3mola <ademolamadelewi@gmail.com>
2019-11-18 13:29:41 +0100

    Updates

---
+++ precache-manifest.fa1afea4925afcfb2fe02596ac7deebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c3332b9a68416f0ba0fbef72958a71",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "23bb58d571f93187096d",
+    "url": "/react-contact-manager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "385a56a02d5d2adddf7d",
+    "url": "/react-contact-manager/static/css/main.dde27da6.chunk.css"
+  },
+  {
+    "revision": "23bb58d571f93187096d",
+    "url": "/react-contact-manager/static/js/2.26df7a75.chunk.js"
+  },
+  {
+    "revision": "385a56a02d5d2adddf7d",
+    "url": "/react-contact-manager/static/js/main.b5b74ac5.chunk.js"
+  },
+  {
+    "revision": "fbcabff2a5b5854bee47",
+    "url": "/react-contact-manager/static/js/runtime-main.1a2cc2e0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a27cdfb5a90149c1e80cd531722f0786.js"
+  "/react-contact-manager/precache-manifest.fa1afea4925afcfb2fe02596ac7deebd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-contact-manager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
